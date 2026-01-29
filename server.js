dff0c2f443b2bb452f37f1902a319c71aa90e990
Radio95 <108529321+Lastlion@users.noreply.github.com>
2026-01-29 16:31:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,27 +4,24 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-// Dire au serveur de servir le fichier index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Logique du chat
 io.on('connection', (socket) => {
-  console.log('Un utilisateur s\'est connecté');
-
-  socket.on('chat message', (msg) => {
-    // Diffuser le message à tout le monde
-    io.emit('chat message', msg);
+  // On prévient tout le monde qu'un utilisateur arrive
+  socket.on('new user', (pseudo) => {
+    socket.pseudo = pseudo;
+    io.emit('chat message', { pseudo: 'Système', text: `${pseudo} a rejoint le chat !` });
   });
 
-  socket.on('disconnect', () => {
-    console.log('Un utilisateur s\'est déconnecté');
+  socket.on('chat message', (data) => {
+    // data contient maintenant { pseudo: "...", text: "..." }
+    io.emit('chat message', data);
   });
 });
 
-// Configurer le port pour Render (important : 0.0.0.0)
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
-  console.log('Serveur démarré sur le port ' + PORT);
+  console.log('Serveur actif sur le port ' + PORT);
 });
