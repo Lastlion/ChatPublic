6ffc114773bc551fbafecd09a842099fade06f2b
Radio95 <108529321+Lastlion@users.noreply.github.com>
2026-01-29 16:49:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const path = require('path');
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 io.on('connection', (socket) => {
-  // On prévient tout le monde qu'un utilisateur arrive
+  // Quand un utilisateur envoie son pseudo juste après la connexion
   socket.on('new user', (pseudo) => {
     socket.pseudo = pseudo;
-    io.emit('chat message', { pseudo: 'Système', text: `${pseudo} a rejoint le chat !` });
+    // On diffuse à TOUT LE MONDE que quelqu'un est arrivé
+    io.emit('chat message', { 
+      pseudo: 'SYSTÈME', 
+      text: `👋 ${pseudo} vient de rejoindre la radio !` 
+    });
   });
 
   socket.on('chat message', (data) => {
-    // data contient maintenant { pseudo: "...", text: "..." }
     io.emit('chat message', data);
   });
-});
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, '0.0.0.0', () => {
-  console.log('Serveur actif sur le port ' + PORT);
+  socket.on('disconnect', () => {
+    if (socket.pseudo) {
+      io.emit('chat message', { 
+        pseudo: 'SYSTÈME', 
+        text: `🚪 ${socket.pseudo} a quitté le chat.` 
+      });
+    }
+  });
 });
